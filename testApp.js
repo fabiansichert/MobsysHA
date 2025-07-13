814e463b77fbfe64241bdbdc4104e29fb8a7f6d5
fabiansichert <fabian.sichert.1@gmx.de>
2025-07-13 10:16:17 +0200

    bugfix: URL

--- testApp.js
+++ testApp.js
@@ -1,9 +1,19 @@
-import { db } from './public/firebase/firebase-config.js';
-import { collection, getDocs } from 'https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import { isAdmin, logTest } from './test.js';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBmqUwSODy1cNOpFvfqDX4XLRPhODqI9No",
+  authDomain: "mobsysha.firebaseapp.com",
+  projectId: "mobsysha",
+  storageBucket: "mobsysha.firebasestorage.app",
+  messagingSenderId: "168308844642",
+  appId: "1:168308844642:web:d58374fdee70af8066893b"
+};
 
-// Integrationstest
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
 async function testFetchNotes() {
   const snap = await getDocs(collection(db, 'notes'));
   console.assert(snap.size >= 0, "Fehler beim Abrufen der Notizen");
